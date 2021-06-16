44f6fb888d29db4b034b12ec3276314de1894f94
이효진 <billion109@naver.com>
2021-06-16 17:14:36 +0900

    Initial commit

---
+++ src/main/java/com/tistory/billion109/commitggspring/CommitggspringApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.billion109.commitggspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitggspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitggspringApplication.class, args);
+    }
+
+}
